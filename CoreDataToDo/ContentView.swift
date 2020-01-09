48fda1989c1d5cac36090cacad50d7a808652ed1
Yoga Kurniawan <yoga.kwn@gmail.com>
2020-01-10 01:33:27 +0700

    Initial Commit

---
+++ CoreDataToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataToDo
+//
+//  Created by Kafilah on 1/10/20.
+//  Copyright © 2020 Yoga Kurniawan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
